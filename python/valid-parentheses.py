558b80b5593ac2999d7994d0472c1dfff6b9bc3f
Tor344 <per7euse@gmail.com>
2026-06-12 12:55:10 +0300

    fix: name in main

--- python/valid-parentheses.py
+++ python/valid-parentheses.py
@@ -5,11 +5,11 @@ staples = {
 }
 
 
-def main(string:str):
-    while string != "":
-        for i in range(len(string) - 1):
-            if staples.get(string[i]) == string[i + 1]:
-                string = string[:i] + string[i + 2:]
+def main(s:str):
+    while s != "":
+        for i in range(len(s) - 1):
+            if staples.get(s[i]) == s[i + 1]:
+                s = s[:i] + s[i + 2:]
                 break
         else:
             return False
